d82f7d1dd82f51f646c43e36d6940c3319163d81
maker79 <vladan.djurkovic@gmail.com>
2020-04-13 20:54:11 -0700

    initial commit

---
+++ src/controller/ModifyCustomerController.java
@@ -0,0 +1,81 @@
+package controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class ModifyCustomerController {
+
+    Stage stage;
+    Parent scene;
+
+    @FXML
+    private Label modifyCustomerLbl;
+
+    @FXML
+    private Label nameLbl;
+
+    @FXML
+    private Label countryLbl;
+
+    @FXML
+    private Label cityLbl;
+
+    @FXML
+    private Label addressLbl;
+
+    @FXML
+    private Label zipCodeLbl;
+
+    @FXML
+    private Label phoneNumberLbl;
+
+    @FXML
+    private TextField nameTxt;
+
+    @FXML
+    private TextField cityTxt;
+
+    @FXML
+    private TextField addressTxt;
+
+    @FXML
+    private TextField zipCodeTxt;
+
+    @FXML
+    private TextField phoneNumberTxt;
+
+    @FXML
+    private ComboBox<?> countryChoiceBox;
+
+    @FXML
+    private Button cancelModifyCustomerBtn;
+
+    @FXML
+    private Button saveModifyCustomerBtn;
+
+    @FXML
+    void onActionCancelModifyCustomer(ActionEvent event) throws IOException {
+
+        stage = (Stage) ((Button) event.getSource()).getScene().getWindow();
+        scene = FXMLLoader.load(getClass().getResource("/view/Customers.fxml"));
+        stage.setScene(new Scene(scene));
+        stage.show();
+
+    }
+
+    @FXML
+    void onActionSaveModifyCustomer(ActionEvent event) {
+
+    }
+
+}
