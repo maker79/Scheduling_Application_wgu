16d5a22105a1acaaf102d97b3c13109e0c442ebc
maker79 <vladan.djurkovic@gmail.com>
2020-04-16 08:26:06 -0700

    initial commit

---
+++ src/model/Appointment.java
@@ -0,0 +1,85 @@
+package model;
+
+public class Appointment {
+
+    private String appointmentId;
+    private Customer customer;
+    private String title;
+    private String description;
+    private String start;
+    private String end;
+    private String user;
+
+    public Appointment() {
+    }
+
+    public Appointment(String appointmentId) {
+        this.appointmentId = appointmentId;
+    }
+
+    public Appointment(String appointmentId, Customer customer, String title, String description, String start, String end, String user) {
+        this.appointmentId = appointmentId;
+        this.customer = customer;
+        this.title = title;
+        this.description = description;
+        this.start = start;
+        this.end = end;
+        this.user = user;
+    }
+
+    public String getAppointmentId() {
+        return appointmentId;
+    }
+
+    public void setAppointmentId(String appointmentId) {
+        this.appointmentId = appointmentId;
+    }
+
+    public Customer getCustomer() {
+        return customer;
+    }
+
+    public void setCustomer(Customer customer) {
+        this.customer = customer;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getStart() {
+        return start;
+    }
+
+    public void setStart(String start) {
+        this.start = start;
+    }
+
+    public String getEnd() {
+        return end;
+    }
+
+    public void setEnd(String end) {
+        this.end = end;
+    }
+
+    public String getUser() {
+        return user;
+    }
+
+    public void setUser(String user) {
+        this.user = user;
+    }
+}
